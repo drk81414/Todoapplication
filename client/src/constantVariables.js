1108dcd144e2d608604acbd5f844ac3f3c5a64e3
Vikrant <vikrant4514@gmail.com>
2024-02-15 00:30:52 +0530

    changed end  points

--- client/src/constantVariables.js
+++ client/src/constantVariables.js
@@ -1,2 +1,4 @@
 // export const Host = 'http://localhost:5000'
-export const Host = 'https://todoapplication-psi.vercel.app/'
+// export const Host = 'https://to-do-application-blush-ten.vercel.app'
+
+export const Host = 'https://todoapplication-psi.vercel.app'
